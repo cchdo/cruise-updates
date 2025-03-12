561ab1584f2be84d2584a43ebca9bd8e966372be
Andre <s.dossantos.andre@gmail.com>
2025-03-12 14:04:46 -0700

    Update how app displays

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import CruiseUpdates from "./CruiseUpdates.tsx";
 function App() {
   return (
     <div className="container mt-4">
-      <h1 className="text-center">Latest Cruise Updates</h1>
       <CruiseUpdates />
     </div>
   );
